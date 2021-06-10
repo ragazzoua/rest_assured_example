591239f068bcc5a0a082c8b90005de8ad3b8307e
Serhii Miroshnychenko <sergey.miroshnychenko@gmail.comgit config --global user.email sergey.miroshnychenko@gmail.com>
2021-06-10 09:02:50 +0300

    project started

---
+++ src/main/java/constants/ApiConstants.java
@@ -0,0 +1,8 @@
+package constants;
+
+public class ApiConstants {
+
+    public static final String BASE_URI = "https://reqres.in/api";
+    public static final String USERS_PATH = "/users";
+    public static final int CODE_200 = 200;
+}
